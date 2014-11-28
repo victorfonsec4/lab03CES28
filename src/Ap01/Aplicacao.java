dcea8106b20b4d9303eba527253cab42ebde2254
Victor Fonseca <victorcel@hotmail.com>
2014-11-28 14:17:17 -0200

    adicionei .gitignore e template da aplicacao.java

--- src/Ap01/Aplicacao.java
+++ src/Ap01/Aplicacao.java
@@ -1,5 +1,24 @@
 package Ap01;
+import ex01.Carrinho;
+import ex01.BancoDeDados;
+import ex01.Produto;
 
 public class Aplicacao {
+
+	BancoDeDados banco = new BancoDeDados();
+
+	// registrar comprador
+	Cliente cliente = Cliente.create(“Zé”, 123);
+	Carrinho car = Carrinho.create();
+	cliente.adicionarCarrinho(car);
+	// realizar uma compra
+	Produto produto = banco.selectProduto(223);
+	cliente.getCarrinho().adicionar(produto);
+	// realizar outra compra
+	Produto produto = banco.selectProduto(342);
+	cliente.getCarrinho().adicionar(produto);
+	// fechar compra
+	double valor = cliente.getCarrinho().getTotal();
+	banco.processarPagamento(cliente, valor);
 
 }
