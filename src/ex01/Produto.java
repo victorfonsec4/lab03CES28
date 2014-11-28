380d9e0bcef7cf20dac0e66f5a72f118198a821d
Victor Fonseca <victorcel@hotmail.com>
2014-11-28 14:57:28 -0200

    criei o produto

--- src/ex01/Produto.java
+++ src/ex01/Produto.java
@@ -1,9 +1,29 @@
 package ex01;
 
 public class Produto {
+	private String nome;
+	private int id;
+	private double preco;
+
+	private Produto(String nome, int id, double preco){
+		this.nome = nome;
+		this.id = id;
+		this.preco = preco;
+	}
+	public static Produto create(String nome, int id, double preco){
+		return new Produto(nome, id, preco);
+	}
 
 	public double getPreco(){
-		return 1;
+		return preco;
+	}
+
+	public String getNome(){
+		return nome;
+	}
+
+	public double getId(){
+		return id;
 	}
 
 }
