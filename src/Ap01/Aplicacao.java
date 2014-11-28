6fd63dd5db8bc7e091ab83190d796e8d9002600e
Victor Fonseca <victorcel@hotmail.com>
2014-11-28 14:32:32 -0200

    corrigi cagado do milton

--- src/Ap01/Aplicacao.java
+++ src/Ap01/Aplicacao.java
@@ -9,7 +9,6 @@ import ex01.Cliente;
 
 public class Aplicacao {
 
-<<<<<<< HEAD
 	public Aplicacao() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -28,22 +27,4 @@ public class Aplicacao {
 	double valor = cliente.getCarrinho().getTotal();
 	banco.processarPagamento(cliente, valor);
 	
-=======
-	BancoDeDados banco = new BancoDeDados();
-
-	// registrar comprador
-	Cliente cliente = Cliente.create(“Zé”, 123);
-	Carrinho car = Carrinho.create();
-	cliente.adicionarCarrinho(car);
-	// realizar uma compra
-	Produto produto = banco.selectProduto(223);
-	cliente.getCarrinho().adicionar(produto);
-	// realizar outra compra
-	Produto produto = banco.selectProduto(342);
-	cliente.getCarrinho().adicionar(produto);
-	// fechar compra
-	double valor = cliente.getCarrinho().getTotal();
-	banco.processarPagamento(cliente, valor);
-
->>>>>>> dcea8106b20b4d9303eba527253cab42ebde2254
 }
