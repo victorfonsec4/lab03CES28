361ddaf81f85f410b358b01219c9e4753890552c
Victor Fonseca <victorcel@hotmail.com>
2014-11-28 16:14:59 -0200

    corrigi erros de static

--- src/ex01/Facade.java
+++ src/ex01/Facade.java
@@ -1,17 +1,18 @@
 package ex01;
 
 public class Facade{
+	Mercado mercado = Mercado.create();
 	
 	public void registrar(String nome, int clienteId) {
-		Mercado.registrar(nome, clienteId);
+		mercado.registrar(nome, clienteId);
 	}
 	
 	public void comprar(int prodId, int clienteId) {
-		Mercado.comprar(prodId, clienteId);
+		mercado.comprar(prodId, clienteId);
 	}
 	
 	public void fecharCompra(int clienteId) {
-		fecharCompra(clienteId);
+		mercado.fecharCompra(clienteId);
 	}
  
 }
